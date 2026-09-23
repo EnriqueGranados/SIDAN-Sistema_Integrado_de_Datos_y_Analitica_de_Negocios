0856ae298e216378ff8ee1eb1d35045ce7114be4
Enrique Granados <jkike2001@gmail.com>
2026-09-23 16:14:28 -0600

    chore: Creación del proyecto y configuración del entorno inicial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('title', 'Welcome')
+
+@section('content')
+    <div class="mx-auto max-w-7xl p-6">
+
+        <h1 class="mb-6 text-3xl font-bold text-gray-900">
+            Welcome
+        </h1>
+
+        <div class="rounded-lg bg-white p-6 shadow">
+            <p class="text-gray-600">
+                Bienvenido a SIDAN.
+            </p>
+        </div>
+    </div>
+@endsection
